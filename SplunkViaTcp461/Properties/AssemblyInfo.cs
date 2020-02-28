319772593cbaea7d043a4b6fbc4581052476b76c
Gauthier <gauthier.rossion@flexiconsult.be>
2020-02-28 11:50:16 +0100

    A simplistic test for Serilog to splunk

---
+++ SplunkViaTcp461/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SplunkViaTcp461")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SplunkViaTcp461")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e021ce43-a1ee-4b61-83f4-98ff08e04ffe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SplunkViaTcp461/SlunkViaTCPTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serilog;
+using Serilog.Sinks.Splunk;
+
+namespace SplunkViaTcp461
+{
+    [TestClass]
+    public class SlunkViaTCPTests
+    {
+        [TestMethod]
+        public void CanLogToSplunk()
+        {
+            var ip = Dns.GetHostAddresses("vs-syslogelogdev.corp.belgrid.net").First();
+            var splunkTcpSinkConnectionInfo = new SplunkTcpSinkConnectionInfo(ip, 514);
+            var loggerConfiguration = new LoggerConfiguration().WriteTo.SplunkViaTcp(splunkTcpSinkConnectionInfo);
+            var logger = loggerConfiguration.CreateLogger();
+            logger.Warning($"Dummy message sent @ {DateTime.Now:O}");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        [Timeout(10000)]
+        public void AnExceptionIsThrownWhenNoSplunk()
+        {
+            var ip = IPAddress.Parse("127.0.0.1");
+            var splunkTcpSinkConnectionInfo = new SplunkTcpSinkConnectionInfo(ip, 514);
+            var loggerConfiguration = new LoggerConfiguration().WriteTo.SplunkViaTcp(splunkTcpSinkConnectionInfo);
+            var logger = loggerConfiguration.CreateLogger();
+            logger.Warning($"Dummy message sent @ {DateTime.Now:O}");
+        }
+
+    }
+}
